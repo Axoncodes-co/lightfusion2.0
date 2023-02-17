a458cb37c28a06940c9c93ef9994eeebaf1f9ef9
Axoncodes <devops@axoncodes.com>
2023-02-17 23:28:51 +0330

    update text to text2 and bring out the searchbar and the continue to read out of their subparent element

--- fragments/Header/index.jsx
+++ fragments/Header/index.jsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic'
 import { useState } from 'react'
 const Searchbar = dynamic(() => import('../../axg-react/Searchbar3'), {ssr: false})
 const Logo = dynamic(() => import('../../axg-react/Logo'), {ssr: false})
-const Text = dynamic(() => import('../../axg-react/Text'), {ssr: false})
+const Text = dynamic(() => import('../../axg-react/Text2'), {ssr: false})
 const Menu = dynamic(() => import('../../axg-react/Menu'), {ssr: false})
 const DropdownBody = dynamic(() => import('../../axg-react/DropdownBody'), {ssr: false})
 
@@ -63,7 +63,15 @@ export default function Header() {
   ])
   return (
     <>
-    <section className={'container horizontal horizontalTabletBreak padding_l0 widePadding_l1'}>
+    <section
+      style={{
+        position: 'sticky',
+        top: 0,
+        zIndex: 9,
+        backgroundColor: '#fff',
+        height: '8vh'
+      }}
+      className={'container horizontal horizontalTabletBreak padding_l0 widePadding_l1'}>
       <section className={'subcontainer horizontal horizontalTabletBreak fitWidth'}>
         <Logo
           src={'/logo.png'}
@@ -76,42 +84,40 @@ export default function Header() {
           menuItems={menuItems}
         />
       </section>
-      <section className={'subcontainer horizontal horizontalTabletBreak fitWidth'}>
-        <Searchbar
-          id={'main_searchbar'}
-          dev={'staging'}
-          name={'main_searchbar'}
-          inputcustomclasses={'font_l1 wide padding_l3 noborder round_l3'}
-          customclasses={'wide'}
-          bg={'#c1c1c1a3'}
-          color={'#000'}
-          placeholder={'Search whatever the fuck you need...'}
-          queryid={'mainsearchquery'}
-          collapseonmobile={'1'}
-          labelclasses={'subcontainer lefty hoversearchcoverlabel'}
-          inputcovercustomclasses={'subcontainer vertical'}
-          reslistcustomclasses={'boxshadow darker rtl'}
-          outformclasses={'searchbarsizes'}
-          searchquerynames={['name1', 'name2']}
-          searchquerylinks={['link1', 'link2']}
+      <Searchbar
+        id={'main_searchbar'}
+        dev={'staging'}
+        name={'main_searchbar'}
+        inputcustomclasses={'font_l1 wide padding_l3 noborder round_l3'}
+        customclasses={'wide'}
+        bg={'#c1c1c1a3'}
+        color={'#000'}
+        placeholder={'Search whatever the fuck you need...'}
+        queryid={'mainsearchquery'}
+        collapseonmobile={'1'}
+        labelclasses={'subcontainer lefty hoversearchcoverlabel'}
+        inputcovercustomclasses={'subcontainer vertical'}
+        reslistcustomclasses={'boxshadow darker rtl'}
+        outformclasses={'searchbarsizes'}
+        searchquerynames={['name1', 'name2']}
+        searchquerylinks={['link1', 'link2']}
+      />
+      <div id={'continue_to_article'}>
+        <Text
+          text={'Continue to Article'}
+          textcolor={'#565969'}
+          textfontsize={'var(--l1-text-fontSize)'}
+          textclasses={`nomargin`}
+          subtext={'the article link'}
+          subtextclasses={`nomargin weight_l9`}
+          subtextcolor={'#001659'}
+          subtextfontsize={'var(--l3-text-fontSize)'}
+          innercustomclasses={'norowgapOnMobile nocolgap vertical verticalOnTablet'}
+          orderdirection={'horizontal'}
+          customclasses={'topy fitWidth'}
+          link={'/'}
         />
-        <div id={'continue_to_article'}>
-          <Text
-            text={'Continue to Article'}
-            textcolor={'#565969'}
-            textfontsize={'var(--l1-text-fontSize)'}
-            textclasses={`nomargin`}
-            subtext={'the article link'}
-            subtextclasses={`nomargin weight_l9`}
-            subtextcolor={'#001659'}
-            subtextfontsize={'var(--l3-text-fontSize)'}
-            innercustomclasses={'nocolgap vertical verticalOnTablet'}
-            orderdirection={'horizontal'}
-            customclasses={'topy fitWidth'}
-            link={'/'}
-          />
-        </div>
-      </section>
+      </div>
     </section>
     <section className="ax_elements" nomain="true">
       {menuItems.map((item, key) => (
