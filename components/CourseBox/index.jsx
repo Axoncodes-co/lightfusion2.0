403d0cb65383dc9ece629148af24025156e4eaf2
Axoncodes <devops@axoncodes.com>
2023-02-22 19:11:03 +0330

    completed the courses accordion(mainly)

---
+++ components/CourseBox/index.jsx
@@ -0,0 +1,40 @@
+
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
+import style from './style.module.css'
+const Text = dynamic(() => import('../../axg-react/Text2'), {ssr: false})
+
+export default function CourseBox({
+    svg,
+    title,
+    description,
+    link,
+    count,
+    courseAttitude
+}) {
+    return (
+        <section className={`${style.outer} subcontainer vertical round_l3 fitWidth`}>
+            <Link href={link} className={`${style.link} padding_l0 subcontainer vertical round_l3`}>
+                <article className={`${style.inner} round_l3`}>
+                    <div className={'subcontainer primary_bg round_l2 widePadding_l2'}>
+                        <Text text={'Free Course'} textclasses={'tertiary_color font_l1 nomargin weight_l4'} />
+                    </div>
+                    <Text
+                        dev={'staging'}
+                        svg={svg}
+                        text={title}
+                        textclasses={'horizontal tertiary_color font_l3 nomargin weight_l6'}
+                        customclasses={'horizontalTabletBreak padding_l1 widePadding_l2 horizontal spread colgap_l3'}
+                    />
+                    <div className={'subcontainer horizontal horizontalTabletBreak primary_bg round_l2 widePadding_l2'}>
+                        <Text text={'Beginner Friendly'} textclasses={'fitWidth tertiary_color font_l1 nomargin weight_l4'} />
+                        <div className={'subcontainer horizontal horizontalTabletBreak fitWidth'}>
+                            <Text text={'10'} textclasses={'tertiary_color font_l1 nomargin weight_l4 fitWidth'} />
+                            <Text text={'19'} textclasses={'tertiary_color font_l1 nomargin weight_l4 fitWidth'} />
+                        </div>
+                    </div>
+                </article>
+            </Link>
+        </section>
+    )
+}
---
+++ fragments/CoursesCat/index.jsx
@@ -0,0 +1,76 @@
+
+import style from './style.module.css'
+import dynamic from 'next/dynamic'
+import CourseBox from '../../components/CourseBox'
+import Accordion_head from '../../axg-react/Accordion2/head'
+import Accordion_body from '../../axg-react/Accordion2/body'
+const Accordion = dynamic(() => import('../../axg-react/Accordion2/index'), {ssr: false})
+const Text = dynamic(() => import('../../axg-react/Text2'), {ssr: false})
+
+export default function CoursesCat({categories}) {
+    // TODO: Fit the size of the accordion head
+    return (
+        <section className={'norowgap centerMarge centerMargen container horizontal verticalTabletBreak nocolgap wide'}>
+            {categories.map((category, key) => (
+                <Accordion
+                    key={key}
+                    dev={'staging'}
+                    customclasses={`${style.cover} horizontal verticalTabletBreak fitWidth wideonTablet`}
+                    groupname={'popularcourse'}
+                    active={key == 0 ? 'active' : ''}
+                >
+                    <Accordion_head
+                        dev={'staging'}
+                        customclasses={'fitWidth wideonTablet'}
+                        innerclasses={'vertical horizontalTabletBreak spread round_l3'}
+                        bg={'var(--primaryColor)'}
+                    >
+                        <Text
+                            text={category.title}
+                            svg={category.svg}
+                            customclasses={'fitWidthOnTablet fitminWidth textcenter center padding_l3 vertical horizontalTabletBreak'}
+                            textclasses={'tertiary_color nomargin font_l3_min weight_l6'}
+                        />
+                        <section className={'subcontainer center horizontal padding_l3 verticalTabletBreak fitminWidth'}>
+                            <div className={'subcontainer horizontal tooltip_parent center'}>
+                                <Text text={'3'} customclasses={'center'} textclasses={'tertiary_color nomargin font_l1 weight_l6'} />
+                                <Text text={'Courses'} customclasses={'tooltip_content'} textclasses={'tertiary_color nomargin font_l1 weight_l6'} />
+                            </div>
+                            <div className={'subcontainer horizontal tooltip_parent'}>
+                                <Text text={'31'} customclasses={'center'} textclasses={'tertiary_color nomargin font_l1 weight_l6'} />
+                                <Text text={'Lessons'} customclasses={'tooltip_content lefty'} textclasses={'tertiary_color nomargin font_l1 weight_l6'} />
+                            </div>
+                            <div className={'subcontainer horizontal tooltip_parent'}>
+                                <Text text={'131'} customclasses={'center'} textclasses={'tertiary_color nomargin font_l1 weight_l6'} />
+                                <Text text={'Hours'} customclasses={'tooltip_content lefty'} textclasses={'tertiary_color nomargin font_l1 weight_l6'} />
+                            </div>
+                        </section>
+                    </Accordion_head>
+                    <Accordion_body
+                        dev={'staging'}
+                        customclasses={'padding_l0'}
+                        innercustomclasses={'subcontainer vertical nocolgap'}
+                    >
+                        {category.courses.map(({
+                            svg,
+                            title,
+                            description,
+                            link,
+                            count,
+                            courseAttitude
+                        }, key) => <CourseBox
+                                key={key}
+                                svg={svg}
+                                title={title}
+                                description={description}
+                                link={link}
+                                count={count}
+                                courseAttitude={courseAttitude}
+                            />
+                        )}
+                    </Accordion_body>
+                </Accordion>
+            ))}
+        </section>
+    )
+}
