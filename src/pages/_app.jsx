707c039578dafef9c20cbfd96a2475be40f4986c
Axoncodes <devops@axoncodes.com>
2023-03-14 22:23:57 +0330

    axg url

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -217,8 +217,8 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
       <Footer />
       <ActivationHandler />
-      <Script src={'http://localhost:3012/init/v5/dev'} type={'module'} strategy={"beforeInteractive"}></Script>
-      <Script src={'http://localhost:3012/global/runScripts'} strategy={'afterInteractive'}></Script>
+      <Script src={'https://beta.axg.axoncodes.com/init/v5/dev'} type={'module'} strategy={"beforeInteractive"}></Script>
+      <Script src={'https://beta.axg.axoncodes.com/global/runScripts'} strategy={'afterInteractive'}></Script>
       <Script src={'/activationhandlerutils.js'} strategy={"beforeInteractive"}></Script>
       <Axg />
   </>)
