0c5f09438a2e1fa4bef19627de4fecbdaad0c6b0
Axoncodes <devops@axoncodes.com>
2023-03-20 16:47:35 +0330

    complete breadcrumb

--- src/pages/[category_slug]/[course_slug]/[lesson_slug]/index.jsx
+++ src/pages/[category_slug]/[course_slug]/[lesson_slug]/index.jsx
@@ -2,75 +2,88 @@ import dynamic from 'next/dynamic'
 import Link from 'next/link'
 import fetchup from '../../../../../lib/fetch'
 const SectionTitle = dynamic(() => import('../../../../../axg-react/SectionTitle'), {ssr: false})
-const DropdownBody = dynamic(() => import('../../../../../axg-react/DropdownBody'), {ssr: false})
-const DropdownHead = dynamic(() => import('../../../../../axg-react/DropdownHead'), {ssr: false})
+// const DropdownBody = dynamic(() => import('../../../../../axg-react/DropdownBody5'), {ssr: false})
+// const DropdownHead = dynamic(() => import('../../../../../axg-react/DropdownHead5'), {ssr: false})
+const Breadcrumb = dynamic(() => import('../../../../../axg-react/Breadcrumb'), {ssr: false})
 import Header from '../../../../../fragments/Header'
 import Navbar from '../../../../../fragments/Navbar'
 
-export default function Post({ categories, course_slug, course, lesson }) {
+export default function Post({ categories, course_slug, category, course, lesson }) {
     return (
         <>
             <Header categories={categories} />
 			<Navbar data={categories} current_slug={course_slug} />
             <section style={{height: '25vw'}}>
                 {/* breadcrumb */}
-                <div>
-                    <Link href={'/'}>Home</Link>
-                    /
-                    <DropdownHead
-                        color={'red'}
-                        colorHover={'blue'}
-                        activeBackground={'#0000'}
-                        headBackground={'#0000'}
-                        headBackgroundHover={'#0000'}
-                        structure={'simple'}
-                        title={'course.title'}
-                        headtitle={'title'}
-                        // link={`/${category.slug}/${course.slug}`}
-                        dropdownid={'breadcrumb_course_id'}
-                        targetLocator={'breadcrumb_course'}
-                        subtrigger={'click'}
-                        subopening={'sub'}
-                        background={'green'}
-                        dir={'ltr'}
-                    />
-                    <section className="ax_elements" nomain="true">
-                        <DropdownBody
-                            structure={'simple'}
-                            title={'course.title'}
-                            dropdownid={'breadcrumb_course_id'}
-                            targetLocator={'breadcrumb_course'}
-                            subtrigger={'click'}
-                            subopening={'sub'}
-                            background={'var(--dark)'}
-                            options={
-                                categories.map(category => ({
-                                    title: category.title,
-                                    url: `/${category.slug}`,
-                                    level: 'undertab',
-                                    color: '#e6e6e6',
-                                    colorHover: '#fff',
-                                    fontsize: 'var(--l4-text-fontSize)',
-                                    dir: 'rtl',
-                                    activeBg: 'var(--secondaryColor)',
-                                }))
-                            //     [
-                                
-                            //     {
-                            //         title: 'سیمان تهران پاکتی',
-                            //         url: '/shop/tehran-cement',
-                            //         level: 'undertab',
-                            //         color: 'var(--secondaryColor)',
-                            //         fontsize: 'var(--l4-text-fontSize)',
-                            //         dir: 'rtl',
-                            //         activeBg: 'var(--secondaryColor)',
-                            //     },
-                            // ]
-                        }
-                        />
-                    </section>
-                    / {course.title}
-                </div>
+                <Breadcrumb
+                    dev={'staging'}
+                    primaryText={{
+                        text: 'Home',
+                        link: '/',
+                        textclasses: 'weight_l3 font_l3 secondary_font nomargin secondary_color tertiary_color_hover',
+                        customclasses: 'fitWidth',
+                    }}
+                    dropdownone_head={{
+                        dev: 'staging',
+                        text: {
+                            text: category.title,
+                            textclasses: 'weight_l3 font_l3 secondary_font nomargin secondary_color tertiary_color_hover',
+                        },
+                        structure: 'simple',
+                        dropdownid: 'breadcrumb_category_id',
+                        targetLocator: 'breadcrumb_category',
+                        subtrigger: 'click',
+                        subopening: 'sub',
+                        listclasses: 'tertiary_bg fitWidth round_l2 padding_l1',
+                        bodyclasses: 'fitWidth',
+                        options: categories.map(category => ({
+                            level: 'undertab',
+                            text: {
+                                text: category.title,
+                                link: `/${category.slug}`,
+                                textclasses: 'weight_l3 font_l3 secondary_font nomargin primary_color tertiary_color_hover',
+                            },
+                        })),
+                    }}
+                    dropdownone_body={{
+                        dev: 'staging',
+                        structure: 'simple',
+                        dropdownid: 'breadcrumb_category_id',
+                        targetLocator: 'breadcrumb_category',
+                        subtrigger: 'click',
+                        subopening: 'sub',
+                    }}
+                    dropdowntwo_head={{
+                        dev: 'staging',
+                        text: {
+                            text: course.title,
+                            textclasses: 'weight_l3 font_l3 secondary_font nomargin secondary_color tertiary_color_hover',
+                        },
+                        structure: 'simple',
+                        dropdownid: 'breadcrumb_course_id',
+                        targetLocator: 'breadcrumb_course',
+                        subtrigger: 'click',
+                        subopening: 'sub',
+                        listclasses: 'tertiary_bg fitWidth round_l2 padding_l1',
+                        bodyclasses: 'fitWidth',
+                        options: category.courses.map(course => ({
+                            level: 'undertab',
+                            text: {
+                                text: course.title,
+                                link: `/${category.slug}/${course.slug}`,
+                                textclasses: 'weight_l3 font_l3 secondary_font nomargin primary_color tertiary_color_hover',
+                            },
+                        })),
+                    }}
+                    dropdowntwo_body={{
+                        dev: 'staging',
+                        structure: 'simple',
+                        dropdownid: 'breadcrumb_course_id',
+                        targetLocator: 'breadcrumb_course',
+                        subtrigger: 'click',
+                        subopening: 'sub',
+                    }}
+                />
                 <div
                     style={{
                         height: '25vw',
