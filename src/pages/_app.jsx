183006a6719f33b0cfe9fea47527475576457977
Axoncodes <devops@axoncodes.com>
2023-03-31 16:09:41 +0330

    axg url update

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,8 +6,8 @@ export default function App({ Component, pageProps }) {
   return (<>
       <Component {...pageProps} />
       <ActivationHandler />
-      <Script src={'https://staging.axg.axoncodes.com/init/v5'} type={'module'} strategy={"beforeInteractive"}></Script>
-      <Script src={'https://staging.axg.axoncodes.com/global/runScripts'} strategy={'afterInteractive'}></Script>
+      <Script src={'https://axg.axoncodes.com/init/v5'} type={'module'} strategy={"beforeInteractive"}></Script>
+      <Script src={'https://axg.axoncodes.com/global/runScripts'} strategy={'afterInteractive'}></Script>
       <Script src={'/activationhandlerutils.js'} strategy={"beforeInteractive"}></Script>
       <Script src={'/switchThemescheme.js'} strategy={"beforeInteractive"}></Script>
       <Script async src={"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5146054383186265"} crossorigin={"anonymous"}></Script>
