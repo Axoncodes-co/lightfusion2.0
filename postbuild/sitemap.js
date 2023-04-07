0e39e3b0ef3a274e56c7c06e88ae062c45a45338
Axoncodes <devops@axoncodes.com>
2023-04-07 16:53:40 +0330

    add images to sitemap

--- postbuild/sitemap.js
+++ postbuild/sitemap.js
@@ -15,7 +15,13 @@ categories.forEach(category => {
   category.courses.forEach(course => {
     urls.push({url: `/${category.slug}/${course.slug}`, changefreq: 'monthly', priority: 0.8})
     course.lessons.forEach(lesson => {
-      urls.push({url: `/${category.slug}/${course.slug}/${lesson.slug}`, changefreq: 'monthly', priority: 0.8, redirect: `/${category.slug}/${lesson.slug}`})
+      urls.push({
+        url: `/${category.slug}/${course.slug}/${lesson.slug}`,
+        changefreq: 'monthly',
+        priority: 0.8,
+        redirect: `/${category.slug}/${lesson.slug}`,
+        imageslist: lesson.imageslist
+      })
     })
   })
 })
@@ -31,13 +37,21 @@ categories.forEach(category => {
 // })
 
 
-let xmlContent = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`
+let xmlContent = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">`
 // pages
 urls.forEach(url => {
   const link = `https://homapilot.com${url.url}/`
   xmlContent += `<url>\n`
   xmlContent += ` <loc>${link}</loc>\n`
   xmlContent += ` <priority>${url.priority}</priority>\n`
+  if (url.imageslist) {
+    url.imageslist.forEach(image => {
+      xmlContent += `
+        <image:image>
+          <image:loc>https://homapilot.com${image}</image:loc>
+        </image:image>\n`
+    })
+  }
   xmlContent += `</url>\n`
 })
 
