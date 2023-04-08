17632104f233a5008f4bb1b3ac080f28808257ac
Axoncodes <devops@axoncodes.com>
2023-04-09 02:29:42 +0330

    update ads

--- src/pages/[category_slug]/[course_slug]/[lesson_slug]/index.jsx
+++ src/pages/[category_slug]/[course_slug]/[lesson_slug]/index.jsx
@@ -95,8 +95,8 @@ export default function Post({ categories, course_slug, category, course, lesson
                     />
                     <main id='content' className={`${style.content}`} dangerouslySetInnerHTML={{__html: lesson.content}}></main>
                     {/* <!-- /22901649087/contentButtom --> */}
-                    <div id='div-gpt-ad-1680990353679-0' style={{minWidth: '120px', minHeight: '20px', maxWidth: '100vw'}}>
-                        <script dangerouslySetInnerHTML={{__html: `googletag.cmd.push(function() { googletag.display('div-gpt-ad-1680990353679-0'); });`}} />
+                    <div id='div-gpt-ad-1680994690027-0' style={{minWidth: '250px', minHeight: '250px'}}>
+                        <script dangerouslySetInnerHTML={{__html: `googletag.cmd.push(function() { googletag.display('div-gpt-ad-1680994690027-0'); });`}} />
                     </div>
                 </section>
                 <aside>
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -38,7 +38,7 @@ export default function Document() {
           googletag.cmd.push(function() {
             googletag.defineSlot('/22901649087/sidebar', [[120, 600], [200, 446], [240, 400], [120, 240]], 'div-gpt-ad-1680994203642-0').addService(googletag.pubads());
             googletag.defineSlot('/22901649087/sidebar2', [[120, 240], [120, 600], [200, 446], [240, 400]], 'div-gpt-ad-1680994309478-0').addService(googletag.pubads());
-            googletag.defineSlot('/22901649087/contentButtom', [[980, 120], [120, 20], [980, 90], [320, 100], [300, 75], [728, 90], [480, 320], [320, 50], [468, 60], [970, 90]], 'div-gpt-ad-1680990353679-0').addService(googletag.pubads());
+            googletag.defineSlot('/22901649087/contentButtom', [[250, 250], [300, 250]], 'div-gpt-ad-1680994690027-0').addService(googletag.pubads())
             googletag.pubads().enableSingleRequest();
             googletag.enableServices();
           });
