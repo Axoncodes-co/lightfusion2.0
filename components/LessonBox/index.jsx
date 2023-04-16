752650b0e9ce061849c223e294e3fce0ee56e714
Axoncodes <devops@axoncodes.com>
2023-04-16 22:40:38 +0330

    init setup

--- components/LessonBox/index.jsx
+++ components/LessonBox/index.jsx
@@ -3,7 +3,15 @@ import Link from 'next/link'
 import style from './style.module.css'
 import Text from '../../builtin-axg/text/v2'
 
-export default function LessonBox({data, link, customclasses}) {
+export default function LessonBox({
+    thumbnail_url,
+    tags,
+    updateDate,
+    title,
+    publishDate,
+    link,
+    customclasses
+}) {
     return (
         <article className={`subcontainer vertical round_l3 ${customclasses}`}>
             <Link
@@ -12,7 +20,7 @@ export default function LessonBox({data, link, customclasses}) {
             >
                 <section className={`${style.inner} transition round_l3 subcontainer spread padding_l3`}
                     style={{
-                        backgroundImage: `linear-gradient(0deg, #000000ba, #00000066, #0001, #0002), url(${data.thumbnail_url || ''})`,
+                        backgroundImage: `linear-gradient(0deg, #000000ba, #00000066, #0001, #0002), url(${thumbnail_url || ''})`,
                         backgroundSize: 'cover',
                         backgroundRepeat: 'no-repeat',
                         backgroundPosition: 'center',
@@ -20,16 +28,16 @@ export default function LessonBox({data, link, customclasses}) {
                 >
                     {/* Tag */}
                     <section className={'subcontainer round_l2 rowgap_l1 colgap_l1'}>
-                        {data.tags.map((tag, key) => key > 1 ? '' : <Text key={key} /*link={`/tag/${tag.slug}`}*/ text={tag.name} customclasses={`${style.opacity} tertiary_bg fitWidth round_l2`} textclasses={'secondary_font fitWidth primary_color font_l1 nomargin weight_l4 padding_slight'} />)}
+                        {tags.map((tag, key) => key > 1 ? '' : <Text key={key} /*link={`/tag/${tag.slug}`}*/ text={tag.Title} customclasses={`${style.opacity} tertiary_bg fitWidth round_l2`} textclasses={'secondary_font fitWidth primary_color font_l1 nomargin weight_l4 padding_slight'} />)}
                     </section>
                     <section className={'subcontainer vertical norowgap lefty'}>
                         <div className={'subcontainer horizontal horizontalTabletBreak fitWidth'}>
                             {/* Publish Date */}
-                            <Text text={data.updateDate || data.publishDate} textclasses={'primary_color fitWidth font_l1 nomargin weight_l5'} />
+                            <Text text={updateDate || publishDate} textclasses={'primary_color fitWidth font_l1 nomargin weight_l5'} />
                             {/* <Text text={'|'} textclasses={'fitWidth primary_color font_l1 nomargin weight_l4'} /> */}
                         </div>
                         <Text
-                            text={data.title}
+                            text={title}
                             textclasses={'horizontal primary_color font_l6_min nomargin weight_l6'}
                             customclasses={`${style.title} horizontalTabletBreak padding_l0 horizontal spread colgap_l3`}
                         />
--- fragments/Footer/index.jsx
+++ fragments/Footer/index.jsx
@@ -63,7 +63,7 @@ export default function Footer({categories}) {
                     <Image alt={'socialmedia'} src={'/email.svg'} width={20} height={20} />
                 </Link>
             </div>
-            <p className={'centerOnTablet font_l2_min primary_color righty weight_l3 wideonTablet'}>© 2020–2023 HomaPilot LLC & <a target="_blank" href="https://axoncodes.com" className="tertiary_color">Axoncodes</a> - Made with <svg className={style.hearticon} xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" fill="#AD1457"><path d="m439 900-53-49Q262 736 171 631.5T80 413q0-90 60.5-150.5T290 202q51 0 101 24.5t89 80.5q44-56 91-80.5t99-24.5q89 0 149.5 60.5T880 413q0 114-91 218.5T574 851l-53 49q-17 16-41 16t-41-16Z"/></svg> in Arizona</p>
+            <p className={'centerOnTablet font_l2_min primary_color righty weight_l3 wideonTablet'}>© 2020–2023 HomaPilot LLC & <Link target="_blank" href="https://axoncodes.com" className="tertiary_color">Axoncodes</Link> - Made with <svg className={style.hearticon} xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" fill="#AD1457"><path d="m439 900-53-49Q262 736 171 631.5T80 413q0-90 60.5-150.5T290 202q51 0 101 24.5t89 80.5q44-56 91-80.5t99-24.5q89 0 149.5 60.5T880 413q0 114-91 218.5T574 851l-53 49q-17 16-41 16t-41-16Z"/></svg> in Arizona</p>
         </section>
     </footer>)
 }
