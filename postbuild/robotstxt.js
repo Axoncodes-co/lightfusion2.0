3bfdd5a63d51e5618291a0149896fab6db9cd41c
Axoncodes <devops@axoncodes.com>
2023-06-07 15:46:52 +0330

    make htaccess and robotstxt and sitemap dynamic

--- postbuild/robotstxt.js
+++ postbuild/robotstxt.js
@@ -1,8 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
-const robotsContent = process.env.PRODUCTION == "true"
-  ? fs.readFileSync(path.join(__dirname, '../robots/production-robots.txt'))
-  : fs.readFileSync(path.join(__dirname, '../robots/production-robots.txt'))
+const robotsContent = process.env.PRODUCTION == "true" ? productionRobotstxt() : stagingRobotstxt()
+function productionRobotstxt() {
+  let content = ''
+  content += 'User-agent: *\n'
+  content += 'Allow: /\n'
+  content += `Sitemap: ${process.env.DOMAIN}/sitemap.xml`
+  return content
+}
 
+function stagingRobotstxt() {
+  let content = ''
+  content += 'User-agent: *\n'
+  content += 'Disallow: /'
+  return content
+}
 fs.writeFileSync(path.join(__dirname, '../public/robots.txt'), robotsContent)
